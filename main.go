04343d7b31988806b8a201db1895516cf0ea864b
syuparn <s.hello.spagetti@gmail.com>
2022-10-30 09:28:15 +0900

    add db

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	sqle "github.com/dolthub/go-mysql-server"
+	"github.com/dolthub/go-mysql-server/memory"
 	"github.com/dolthub/go-mysql-server/server"
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/information_schema"
@@ -15,15 +16,21 @@ var (
 	port = flag.Int("port", 3306, "port to listen")
 	user = flag.String("user", "", "create user other than root")
 	pass = flag.String("pass", "", "create password of created user")
+	db   = flag.String("db", "", "create default database")
 )
 
 func main() {
 	flag.Parse()
 
-	engine := sqle.NewDefault(
-		sql.NewDatabaseProvider(
-			information_schema.NewInformationSchemaDatabase(),
-		))
+	dbs := []sql.Database{
+		information_schema.NewInformationSchemaDatabase(),
+	}
+
+	if *db != "" {
+		dbs = append(dbs, memory.NewDatabase(*db))
+	}
+
+	engine := sqle.NewDefault(sql.NewDatabaseProvider(dbs...))
 	engine.Analyzer.Catalog.MySQLDb.AddRootAccount()
 
 	if *user != "" {
