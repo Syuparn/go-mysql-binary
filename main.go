b6b635cb44b9f2d2d613b9a3d523fb0e584f85a6
syuparn <s.hello.spagetti@gmail.com>
2022-10-30 09:20:24 +0900

    add user option

--- main.go
+++ main.go
@@ -13,6 +13,8 @@ import (
 var (
 	host = flag.String("host", "localhost", "host to listen")
 	port = flag.Int("port", 3306, "port to listen")
+	user = flag.String("user", "", "create user other than root")
+	pass = flag.String("pass", "", "create password of created user")
 )
 
 func main() {
@@ -23,6 +25,10 @@ func main() {
 			information_schema.NewInformationSchemaDatabase(),
 		))
 	engine.Analyzer.Catalog.MySQLDb.AddRootAccount()
+
+	if *user != "" {
+		engine.Analyzer.Catalog.MySQLDb.AddSuperUser(*user, *pass)
+	}
 
 	config := server.Config{
 		Protocol: "tcp",
